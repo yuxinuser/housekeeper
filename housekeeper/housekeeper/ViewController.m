30a76a97d5693417b0cd58baa9e1a2bc935eb5dc
yuxinuser <910374964@qq.com>
2017-05-12 16:01:40 +0800

    背景颜色测试

--- housekeeper/housekeeper/ViewController.m
+++ housekeeper/housekeeper/ViewController.m
@@ -16,12 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+   
     // Dispose of any resources that can be recreated.
 }
 
