724497b0348a888a362f21148eec044678f5833e
yuxinuser <910374964@qq.com>
2017-05-12 15:54:32 +0800

    创建工程

---
+++ housekeeper/housekeeper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  housekeeper
+//
+//  Created by 于鑫 on 17/5/12.
+//  Copyright © 2017年 于鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ housekeeper/housekeeper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  housekeeper
+//
+//  Created by 于鑫 on 17/5/12.
+//  Copyright © 2017年 于鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
